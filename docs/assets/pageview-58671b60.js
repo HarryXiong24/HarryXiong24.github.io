9cf67cc6247b41599f0e9b551c2184cb524a05d5
harryxiong24 <harryxiong24@gmail.com>
2023-06-19 17:17:53 +0800

    feat: new version release

--- docs/assets/pageview-58671b60.js
+++
@@ -1,1 +0,0 @@
-import{M as e}from"./app-dbf2ee88.js";const t=()=>e({serverURL:"https://comment.mrhope.site"});export{t as updatePageview};
---
+++ docs/assets/pageview-8de2d97b.js
@@ -0,0 +1,1 @@
+import{M as e}from"./app-79d257de.js";const t=()=>e({serverURL:"https://comment.mrhope.site"});export{t as updatePageview};
